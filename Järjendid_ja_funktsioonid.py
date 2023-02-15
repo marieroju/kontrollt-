2e7ab49fab9ac26b3ea4007d49d01d102c3b8312
marieroju <120181323+marieroju@users.noreply.github.com>
2023-02-15 15:44:12 +0200

    Add files via upload

---
+++ Järjendid_ja_funktsioonid.py
@@ -0,0 +1,58 @@
+﻿list('список')
+def new_func1():
+    return ['с', 'п', 'и', 'с', 'о', 'к']
+
+def new_func2(new_func1):
+    new_varnew_var = new_func1()
+
+new_func2(new_func1)
+
+def new_func():
+  list=["люди"],["рост"]
+
+new_func()
+list = ["Marek","Samanta","Gabriel","Aleks","Oleg"]
+
+def new_func4():
+    print('Current list {name list}')
+
+new_func4()
+
+new_name = input("Please enter your name:\n")
+
+name_list.append(new_name)
+
+def new_func3():
+    print('Updated name List {name list}')
+
+new_func3()
+
+print('Current high List {high_list}')
+
+num = int(input("Please enteryour high to add to list:\n"))
+
+index = int(input('Please enter the index between 0 and {len(high_list) - 1} to add the number:\n'))
+
+high_list.insert(index, num)
+
+from ipaddress import summarize_address_range
+
+
+print('Updated high List {high_list}')
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.high = high
+ 
+    def __repr__(self):
+        return "" % ("self name", "self high")
+
+2. Gabriel-180cm
+   Oleg-190cm
+   count=0
+   for x in A:
+If 180<x and x<190:
+    count+=1
+    summa+=x
+    print(summa
+          print(sum(A))
